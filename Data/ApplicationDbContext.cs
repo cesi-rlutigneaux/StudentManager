982d63260758a91a90976bac25843b2c9657dda4
Romain Lutigneaux <rlutigneaux@cesi.fr>
2024-07-11 17:31:06 +0200

    feat(add): Add + Get list students

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManager.Models.Entities;
+
+namespace StudentManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using StudentManager.Data;
+
 namespace StudentManager
 {
     public class Program
@@ -8,6 +11,10 @@ namespace StudentManager
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(builder.Configuration.GetConnectionString("StudentManagerConnectionString"))
+            );
 
             var app = builder.Build();
 
